7b9aa27cf5e23556fd97448bc494a4694aff3ada
EC2 Default User <ec2-user@ip-172-31-47-96.ap-south-1.compute.internal>
2025-09-24 16:20:43 +0000

    Add Dockerfile, Jenkinsfile, and Flask app for CI/CD

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,5 +7,5 @@ def home():
     return "Hello from Jenkins CI/CD Pipeline on AWS!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
-~
+    # Keep Flask running on all interfaces for Docker
+    app.run(host='0.0.0.0', port=8080, debug=True)
